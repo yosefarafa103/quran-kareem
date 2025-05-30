8c82bd4a59f43b23046d19657aeb5dbe438c3cfe
Usef <jooyosef173@gmail.com>
2025-05-30 18:36:42 +0200

    Add Page Number In Surahs, Sebha, Update FontSize To Be Globally.

--- src/constants/variables.ts
+++ src/constants/variables.ts
@@ -2,4 +2,9 @@
 
 import { LinkType } from "../types/general";
 
-    export const LINKS: LinkType[] = [{ name: "القران الكريم", route: '/quran' }, { name: "مواقيت الصلاة", route: '/prayer-times' }, { name: "الاذكار", route: '/azkar' },{ name: "السبحة", route: '/sebha' }]
+export const LINKS: LinkType[] = [
+  { name: "القران الكريم", route: "/quran" },
+  { name: "مواقيت الصلاة", route: "/prayer-times" },
+  { name: "الاذكار", route: "/azkar" },
+  { name: "السبحة", route: "/sebha" },
+];
